5add1ab8e78d2481af6294378f82a7a522cf38b1
M.Zarli <16181950+sharukurusu@users.noreply.github.com>
2018-11-27 06:31:57 -0500

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,6 @@
 // Set up MySQL connection.
 const mysql = require("mysql"),
-    connection = mysql.createConnection({
-        host: "127.0.0.1",
-        port: 3306,
-        user: "root",
-        password: "root",
-        database: "burgers_db"
-    });
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 // Make connection.
 connection.connect(function(err) {
