15d1e2d8877d6e5a24f1a945920f696b35b3c4ab
M.Zarli <16181950+sharukurusu@users.noreply.github.com>
2018-11-27 22:06:37 -0500

    module app export3

--- controllers/burgers_Controller.js
+++ controllers/burgers_Controller.js
@@ -1,5 +1,5 @@
-const   express = require("express"),
-        router = express.Router(),
+const express = require("express"),
+        // router = express.Router(),
         burger = require("../models/burger.js");
 
 module.exports = function(app) {
