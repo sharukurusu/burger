39a8f87718e528b3f257f32772cfc603aaf0b982
M.Zarli <16181950+sharukurusu@users.noreply.github.com>
2018-11-27 20:21:35 -0500

    if else connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,15 @@
 // Set up MySQL connection.
 const mysql = require("mysql"),
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    if (process.env.JAWSDB_URL) {connection = mysql.createConnection(process.env.JAWSDB_URL);}
+    else {
+        connection = mysql.createConnection({
+            host: "127.0.0.1",
+            port: 3306,
+            user: "root",
+            password: "root",
+            database: "burgers_db"
+        });}
+    
 
 // Make connection.
 connection.connect(function(err) {
